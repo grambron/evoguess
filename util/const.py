719e9348b194cbb3e41451fbd960bfe8ede3a516
Anastasiia Badikova <nastyabadikova@yandex.ru>
2022-03-23 21:01:34 +0300

    fixing dotenv const

--- util/const.py
+++ util/const.py
@@ -1,6 +1,6 @@
 from dotenv import dotenv_values
 
-const = dotenv_values("/Users/aleksandrpotapov/Documents/Nastia/evoguess-fw/evoguess/.env")
+const = dotenv_values(".env")
 MAIN_PATH = const['MAIN_PATH']
 DATA_PATH = const['DATA_PATH']
 TEMPLATE_PATH = const['TEMPLATE_PATH']
