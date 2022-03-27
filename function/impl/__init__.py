3ea9341076120c5409b2fc6f987bdceed93fa856
Anastasiia Badikova <nastyabadikova@yandex.ru>
2022-03-27 23:12:16 +0300

    upgad_ilp and deleting max_literal

--- function/impl/__init__.py
+++ function/impl/__init__.py
@@ -3,16 +3,19 @@ from .ilp_gad import *
 from .ilp_gad import *
 from .up_gad import *
 from .incr_gad import *
+from .up_gad_ilp import *
 
 functions = {
     GuessAndDetermine.slug: GuessAndDetermine,
     GuessAndDetermineILP.slug: GuessAndDetermineILP,
     UPGuessAndDetermine.slug: UPGuessAndDetermine,
-    IncrGuessAndDetermine.slug: IncrGuessAndDetermine
+    IncrGuessAndDetermine.slug: IncrGuessAndDetermine,
+    UPGuessAndDetermineILP.slug: UPGuessAndDetermineILP
 }
 
 __all__ = [
     'GuessAndDetermine',
     'GuessAndDetermineILP',
-    'UPGuessAndDetermine'
+    'UPGuessAndDetermine',
+    'UPGuessAndDetermineILP'
 ]
