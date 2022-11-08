6dfeafe6ca879c88a6b7d2c8cb62503f3c248768
abadikova <nastyabadikova@yandex.ru>
2022-11-08 13:15:34 +0300

    scip without cython

--- instance/typings/scip_ilp.py
+++ instance/typings/scip_ilp.py
@@ -1,9 +1,8 @@
-import os
 import re
 import threading
 
-from src.pyscipopt.scip import Model
-from util.const import TEMPLATE_PATH
+from pyscipopt.scip import Model
+
 
 ilp_clauses = {}
 lock = threading.Lock()
@@ -15,39 +14,16 @@ class ScipILPClause:
         self.model = model
 
 
+class ScipILPClauseForValidation:
+    def __init__(self, model: Model):
+        self.model = model
+        self.var_index_dict = {}
+
+        counter = 1
+        for var in model.getVars():
+            self.var_index_dict[var.name] = counter
+            counter += 1
+
+
 class ScipILP:
     slug = 'scip_ilp'
-    name = 'SCIP_ILP'
-
-    def __init__(self, path):
-        self.path = os.path.join(TEMPLATE_PATH, path)
-
-    def _parse(self):
-        if self.path in ilp_clauses:
-            return
-
-        model = Model()
-
-        model.readProblem(self.path)
-
-        ilp_clauses[self.path] = ScipILPClause(model)
-
-    def clauses(self):
-        with lock:
-            self._parse()
-            return ilp_clauses[self.path]
-
-    def __copy__(self):
-        return ScipILP(self.path)
-
-    def __info__(self):
-        return {
-            'slug': self.slug,
-            'name': self.name,
-            'path': self.path,
-        }
-
-
-__all__ = [
-    'ScipILP'
-]
